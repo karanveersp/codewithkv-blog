9c90e6bae429d952836e66c249da2045c6eac31c
karanveersp <karan_4496@hotmail.com>
2022-02-16 09:48:06 -0500

    Added simple file logger project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,23 +8,48 @@ const Projects = props => {
     <div>
       <Layout location={props.location}>
         <h2 className="postTitle">Projects</h2> <br />
+        <p></p>
         <ul>
           <li>
             <div>
-              <span><a href="https://github.com/karanveersp/CypherFS">CypherFS</a></span>
+              <span>
+                <a href="https://github.com/karanveersp/CypherFS">CypherFS</a>
+              </span>
               <p>
-                An F# CLI app to AES encrypt/decrypt text data.<br />
-                Useful for storing passwords, and secrets, which can be backed up and shared securely.
+                An F# CLI app to AES encrypt/decrypt text data.
+                <br />
+                Useful for storing passwords, and secrets, which can be backed
+                up and shared securely.
               </p>
             </div>
           </li>
 
           <li>
             <div>
-              <span><a href="https://github.com/karanveersp/Capstone">Cryptocurrency Price Forecaster</a></span>
+              <span>
+                <a href="https://github.com/karanveersp/Capstone">
+                  Cryptocurrency Price Forecaster
+                </a>
+              </span>
               <p>
-                A desktop app to analyze and forecast cryptocurrency prices using machine learning.<br />
-                Build with C#, ML.NET and Avalonia.
+                A desktop app to analyze and forecast cryptocurrency prices
+                using machine learning.
+                <br />
+                Built with C#, ML.NET and Avalonia.
+              </p>
+            </div>
+          </li>
+
+          <li>
+            <div>
+              <span>
+                <a href="https://github.com/karanveersp/simple-file-logger">
+                  Simple File Logger
+                </a>
+              </span>
+              <p>
+                A Python library providing an easy to use class for logging to
+                files with opinionated defaults that can be customized.
               </p>
             </div>
           </li>
